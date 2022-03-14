cf8114a0bbc1674bdce2a7cf1484fbb7c48ef261
Pedro Henrique <coc.cr.ph@gmail.com>
2022-03-13 22:59:59 -0300

    Props

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { Movie } from 'types/movie';
 import './styles.css'
 
-function Form() {
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie : Movie;
+}
+
+function Form({movie} : Props) {
+    
+    const params = useParams();
 
     return (
         <div className="dsmovie-form-container">
